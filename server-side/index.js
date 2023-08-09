f94179b9b634cf5f4bc6b027078a7bef075fbe11
Prodip Karati <104679918+prodipdev@users.noreply.github.com>
2023-08-09 12:55:36 +0600

    Revert "Create Navbar Hero Search-filter Section Prodipdev"

--- server-side/index.js
+++ server-side/index.js
@@ -1,82 +1,16 @@
 const express = require("express");
-const app = express();
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
-require("dotenv").config();
+
+const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
-const verifyJWT = (req, res, next) => {
-  const authorization = req.headers.authorization;
-  if (!authorization) {
-    return res
-      .status(401)
-      .send({ error: true, message: "unauthorized access" });
-  }
-  // bearer token
-  const token = authorization.split(" ")[1];
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res
-        .status(401)
-        .send({ error: true, message: "unauthorized access" });
-    }
-    req.decoded = decoded;
-    next();
-  });
-};
-
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.96fzlxn.mongodb.net/?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    const usersCollection = client.db("airBlissDb").collection("users");
-    const flightsCollection = client.db("airBlissDb").collection("flights");
-    const bookingsCollection = client.db("airBlissDb").collection("bookings");
-
-    app.post("/jwt", (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
-      });
-
-      res.send({ token });
-    });
-
-    // // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
-
 app.get("/", (req, res) => {
-  res.send("AirBliss Server is running..");
+  res.send("Flight Booking Server is now Running");
 });
 
 app.listen(port, () => {
-  console.log(`AirBliss is running on port ${port}`);
+  console.log(`Flight Booking server running on port: ${port}`);
 });
