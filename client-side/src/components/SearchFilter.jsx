8f4f570c16ff43b464b88061d76ecd5a79abc4ec
prodipdev <prodipink@gmail.com>
2023-08-09 21:35:09 +0600

    More dynamic search-filter

--- client-side/src/components/SearchFilter.jsx
+++ client-side/src/components/SearchFilter.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { MdFlight } from "react-icons/md";
+import { MdAddCircle, MdFlight, MdRemoveCircle } from "react-icons/md";
 import { RiHotelFill } from "react-icons/ri";
 import { BsPostcardFill } from "react-icons/bs";
 
 const SearchFilter = () => {
   const [isActive, setIsActive] = useState("flight");
+  const [flightType, setFlightType] = useState("oneWay");
+  const [cityCount, setCityCount] = useState(1);
 
   return (
     <div className="max-w-7xl mx-auto grid justify-center">
@@ -42,6 +44,8 @@ const SearchFilter = () => {
               name="flightType"
               value="oneWay"
               className="radio radio-info"
+              checked={flightType === "oneWay"}
+              onChange={() => setFlightType("oneWay")}
             />
             One Way
           </label>
@@ -51,6 +55,8 @@ const SearchFilter = () => {
               name="flightType"
               value="roundTrip"
               className="radio radio-info"
+              checked={flightType === "roundTrip"}
+              onChange={() => setFlightType("roundTrip")}
             />
             Round Trip
           </label>
@@ -60,88 +66,289 @@ const SearchFilter = () => {
               name="flightType"
               value="multiCity"
               className="radio radio-info"
+              checked={flightType === "multiCity"}
+              onChange={() => setFlightType("multiCity")}
             />
             Multi City
           </label>
         </div>
 
-        <div className="search-grid gap-2 mb-3">
-          <div className="w-full p-2 border rounded-md">
-            <label htmlFor="fromCity">
-              <p className="text-sm">From</p>
-              <input
-                id="fromCity"
-                type="text"
-                className="text-xl font-semibold outline-none"
-                value="Dhaka"
-              />
-              <div className="cursor-pointer">
-                <small className="text-xs my-0">
-                  <span title="" className="">
-                    Hazrat Shahjalal International Airport
-                  </span>
-                </small>
+        <div className="mb-3">
+          {flightType === "multiCity" ? (
+            <>
+              <div className="search-grid gap-2">
+                <div className="w-full p-2 border rounded-md">
+                  <label htmlFor="fromCity">
+                    <p className="text-sm">From</p>
+                    <input
+                      id="fromCity"
+                      type="text"
+                      className="text-xl font-semibold outline-none"
+                      value="Dhaka"
+                    />
+                    <div className="cursor-pointer">
+                      <small className="text-xs my-0">
+                        <span title="" className="">
+                          Hazrat Shahjalal International Airport
+                        </span>
+                      </small>
+                    </div>
+                  </label>
+                </div>
+                <div className="w-full p-2 border rounded-md">
+                  <label htmlFor="toCity">
+                    <p className="text-sm">To</p>
+                    <input
+                      id="toCity"
+                      type="text"
+                      className="text-lg sm:text-xl font-semibold outline-none"
+                      value="Khulna"
+                    />
+                    <div className="cursor-pointer">
+                      <small className="text-xs my-0">
+                        <span title="" className="">
+                          Khulna International Airport
+                        </span>
+                      </small>
+                    </div>
+                  </label>
+                </div>
+                <div className="grid grid-cols-2 border rounded-md">
+                  <div className="w-full p-2 border-r relative overflow-hidden">
+                    <p className="text-sm">Departure</p>
+                    <div className="cursor-pointer">
+                      <input
+                        type="text"
+                        className="text-lg sm:text-xl font-semibold outline-none bg-transparent"
+                        value="08 Aug 23"
+                        readOnly
+                      />
+                      <small className="text-xs my-0">
+                        <span title="" className="">
+                          Tuesday
+                        </span>
+                      </small>
+                    </div>
+                  </div>
+                  <div className="w-full p-2">
+                    <p className="text-sm">Return</p>
+                    <h5 className="text-lg sm:text-xl font-semibold">
+                      11 Aug 23
+                    </h5>
+                    <small className="text-xs my-0">
+                      <span title="" className="">
+                        Friday
+                      </span>
+                    </small>
+                  </div>
+                </div>
+                <div className="w-full p-2 border rounded-md cursor-pointer">
+                  <p className="text-sm">Travel & Booking Class</p>
+                  <h5 className="text-lg sm:text-xl font-semibold">
+                    1 Traveler
+                  </h5>
+                  <small className="text-xs my-0">
+                    <span title="" className="">
+                      Business Class
+                    </span>
+                  </small>
+                </div>
               </div>
-            </label>
-          </div>
-          <div className="w-full p-2 border rounded-md">
-            <label htmlFor="toCity">
-              <p className="text-sm">To</p>
-              <input
-                id="toCity"
-                type="text"
-                className="text-lg sm:text-xl font-semibold outline-none"
-                value="Khulna"
-              />
-              <div className="cursor-pointer">
-                <small className="text-xs my-0">
-                  <span title="" className="">
-                    Khulna International Airport
-                  </span>
-                </small>
+              {Array.from({ length: cityCount }, (_, index) => (
+                <div className="search-grid gap-2 mt-1">
+                  <div className="w-full p-2 border rounded-md">
+                    <label htmlFor="fromCity">
+                      <p className="text-sm">From</p>
+                      <input
+                        id="fromCity"
+                        type="text"
+                        className="text-xl font-semibold outline-none"
+                        value="Dhaka"
+                      />
+                      <div className="cursor-pointer">
+                        <small className="text-xs my-0">
+                          <span title="" className="">
+                            Hazrat Shahjalal International Airport
+                          </span>
+                        </small>
+                      </div>
+                    </label>
+                  </div>
+                  <div className="w-full p-2 border rounded-md">
+                    <label htmlFor="toCity">
+                      <p className="text-sm">To</p>
+                      <input
+                        id="toCity"
+                        type="text"
+                        className="text-lg sm:text-xl font-semibold outline-none"
+                        value="Khulna"
+                      />
+                      <div className="cursor-pointer">
+                        <small className="text-xs my-0">
+                          <span title="" className="">
+                            Khulna International Airport
+                          </span>
+                        </small>
+                      </div>
+                    </label>
+                  </div>
+                  <div className="grid grid-cols-2 border rounded-md">
+                    <div className="w-full p-2 border-r relative overflow-hidden">
+                      <p className="text-sm">Departure</p>
+                      <div className="cursor-pointer">
+                        <input
+                          type="text"
+                          className="text-lg sm:text-xl font-semibold outline-none bg-transparent"
+                          value="08 Aug 23"
+                          readOnly
+                        />
+                        <small className="text-xs my-0">
+                          <span title="" className="">
+                            Tuesday
+                          </span>
+                        </small>
+                      </div>
+                    </div>
+                    <div className="w-full p-2">
+                      <p className="text-sm">Return</p>
+                      <h5 className="text-lg sm:text-xl font-semibold">
+                        11 Aug 23
+                      </h5>
+                      <small className="text-xs my-0">
+                        <span title="" className="">
+                          Friday
+                        </span>
+                      </small>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-center gap-1 text-sm w-full p-2 border rounded-md cursor-pointer">
+                    {index !== cityCount - 1 ? (
+                      <button
+                        className="flex items-center gap-1 text-red-500"
+                        onClick={() => setCityCount(cityCount - 1)}
+                      >
+                        <MdRemoveCircle /> Remove
+                      </button>
+                    ) : (
+                      <>
+                        <button
+                          className="border px-3 py-1 flex items-center text-cyan-500"
+                          onClick={() => setCityCount(cityCount + 1)}
+                        >
+                          <MdAddCircle /> Add
+                        </button>
+                        {index > 0 && (
+                          <>
+                            <div className="divider divider-horizontal"></div>
+                            <button
+                              className="flex items-center gap-1 text-red-500"
+                              onClick={() => setCityCount(cityCount - 1)}
+                            >
+                              <MdRemoveCircle /> Remove
+                            </button>
+                          </>
+                        )}
+                      </>
+                    )}
+                  </div>
+                </div>
+              ))}
+            </>
+          ) : (
+            <div className="search-grid gap-2">
+              <div className="w-full p-2 border rounded-md">
+                <label htmlFor="fromCity">
+                  <p className="text-sm">From</p>
+                  <input
+                    id="fromCity"
+                    type="text"
+                    className="text-xl font-semibold outline-none"
+                    value="Dhaka"
+                  />
+                  <div className="cursor-pointer">
+                    <small className="text-xs my-0">
+                      <span title="" className="">
+                        Hazrat Shahjalal International Airport
+                      </span>
+                    </small>
+                  </div>
+                </label>
               </div>
-            </label>
-          </div>
-          <div className="grid grid-cols-2 border rounded-md">
-            <div className="w-full p-2 border-r relative">
-              <p className="text-sm">Departure</p>
-              <div className="cursor-pointer">
-                <input
-                  type="text"
-                  className="text-lg sm:text-xl font-semibold outline-none bg-transparent"
-                  value="08 Aug 23"
-                  readOnly
-                />
+              <div className="w-full p-2 border rounded-md">
+                <label htmlFor="toCity">
+                  <p className="text-sm">To</p>
+                  <input
+                    id="toCity"
+                    type="text"
+                    className="text-lg sm:text-xl font-semibold outline-none"
+                    value="Khulna"
+                  />
+                  <div className="cursor-pointer">
+                    <small className="text-xs my-0">
+                      <span title="" className="">
+                        Khulna International Airport
+                      </span>
+                    </small>
+                  </div>
+                </label>
+              </div>
+              <div className="grid grid-cols-2 border rounded-md">
+                <div className="w-full p-2 border-r relative overflow-hidden">
+                  <p className="text-sm">Departure</p>
+                  <div className="cursor-pointer">
+                    <input
+                      type="text"
+                      className="text-lg sm:text-xl font-semibold outline-none bg-transparent"
+                      value="08 Aug 23"
+                      readOnly
+                    />
+                    <small className="text-xs my-0">
+                      <span title="" className="">
+                        Tuesday
+                      </span>
+                    </small>
+                  </div>
+                </div>
+                {flightType === "oneWay" && (
+                  <div
+                    className="w-full p-2 cursor-pointer"
+                    onClick={() => setFlightType("roundTrip")}
+                  >
+                    <p className="text-sm">Return</p>
+                    <p className="text-xs my-0 mt-2 cursor-pointer">
+                      Tap to book return ticket for more savings
+                    </p>
+                  </div>
+                )}
+                {flightType === "roundTrip" && (
+                  <div className="w-full p-2">
+                    <p className="text-sm">Return</p>
+                    <h5 className="text-lg sm:text-xl font-semibold">
+                      11 Aug 23
+                    </h5>
+                    <small className="text-xs my-0">
+                      <span title="" className="">
+                        Friday
+                      </span>
+                    </small>
+                  </div>
+                )}
+              </div>
+              <div className="w-full p-2 border rounded-md cursor-pointer">
+                <p className="text-sm">Travel & Booking Class</p>
+                <h5 className="text-lg sm:text-xl font-semibold">1 Traveler</h5>
                 <small className="text-xs my-0">
                   <span title="" className="">
-                    Tuesday
+                    Business Class
                   </span>
                 </small>
               </div>
             </div>
-            <div className="w-full p-2">
-              <p className="text-sm">Return</p>
-              <h5 className="text-lg sm:text-xl font-semibold">11 Aug 23</h5>
-              <small className="text-xs my-0">
-                <span title="" className="">
-                  Friday
-                </span>
-              </small>
-            </div>
-          </div>
-          <div className="w-full p-2 border rounded-md cursor-pointer">
-            <p className="text-sm">Travel & Booking Class</p>
-            <h5 className="text-lg sm:text-xl font-semibold">1 Traveler</h5>
-            <small className="text-xs my-0">
-              <span title="" className="">
-                Business Class
-              </span>
-            </small>
-          </div>
+          )}
         </div>
 
         <div className="flex justify-center -mb-11">
-          <button className="px-10 py-3 rounded bg-cyan-500 active:bg-cyan-600 text-white font-semibold">
+          <button className="px-10 py-3 rounded bg-cyan-600 active:bg-cyan-700 text-white font-semibold">
             Search
           </button>
         </div>
