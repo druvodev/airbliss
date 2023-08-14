8e3394b3848836d882ee748549d456795ed9419a
prodipdev <prodipink@gmail.com>
2023-08-14 11:21:19 +0600

    Create Flights Layout

--- client-side/src/routes/Routes.jsx
+++ client-side/src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
+import Flights from "../pages/Flights/Flights/Flights";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/flights",
+        element: <Flights />,
       },
     ],
   },
