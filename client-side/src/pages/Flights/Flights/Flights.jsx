3ac6b31620b6216413da6ee92ce66b3184234bff
AbdullahWB <abdullah917828@gmail.com>
2023-08-14 20:14:21 +0600

    done sorting by price

--- client-side/src/pages/Flights/Flights/Flights.jsx
+++ client-side/src/pages/Flights/Flights/Flights.jsx
@@ -4,9 +4,9 @@ import ShortingFlight from "../ShortingFlight/ShortingFlight";
 
 const Flights = () => {
   return (
-    <div className="mt-16 max-w-7xl mx-auto">
+    <div className="mt-16 max-w-7xl mb-[250px] mx-auto">
       <SearchFilter />
-      <div className=" grid md:grid-cols-3 gap-5 mt-12 px-5 sm:px-10">
+      <div className=" grid md:grid-cols-3 gap-5 mt-32 px-5 sm:px-10">
         <div className="col-span-1">
           <ResultsFilter />
         </div>
--- client-side/src/pages/Flights/ShortingFlight/ShortingFlight.jsx
+++ client-side/src/pages/Flights/ShortingFlight/ShortingFlight.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaCaretLeft, FaCaretRight } from 'react-icons/fa';
 
 const ShortingFlight = () => {
+    const [selectedButton, setSelectedButton] = useState('cheapest'); // Initialize with the default selected button
+
+    const handleButtonClick = (buttonType) => {
+        setSelectedButton(buttonType);
+    };
+
     return (
         <div>
             <div className='flex justify-between items-center mb-3'>
@@ -13,8 +19,26 @@ const ShortingFlight = () => {
                     <FaCaretRight className='text-[45px] text-cyan-500 cursor-pointer' />
                 </div>
             </div>
-            <div>
-                shorting by price
+            <div className='flex w-full p-5 mt-10 rounded-md justify-between shadow-2xl'>
+                <button
+                    className={`p-4 text-left flex-grow py-2 px-3 pe-5 mb-0 border-0 ${
+                        selectedButton === 'cheapest' ? 'bg-gray-100 text-white' : 'text-white'
+                    }`}
+                    onClick={() => handleButtonClick('cheapest')}
+                >
+                    <h1 className='text-[18px] font-semibold mb-2 text-gray-900'>Cheapest</h1>
+                    <p className='text-[14px] text-[#7c8db0]'>To get the cheapest available flights</p>
+                </button>  
+                <div className='border self-stretch mx-5'></div>
+                <button
+                    className={`p-4 text-left flex-grow py-2 px-3 pe-5 mb-0 border-0 ${
+                        selectedButton === 'shortest' ? 'bg-gray-100 text-white' : 'text-white'
+                    }`}
+                    onClick={() => handleButtonClick('shortest')}
+                >
+                    <h1 className='text-[18px] font-semibold mb-2 text-gray-900'>Shortest</h1>
+                    <p className='text-[14px] text-[#7c8db0]'>To get the shortest available flights</p>
+                </button>
             </div>
             <div>
                 shorting by company
