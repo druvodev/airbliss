4570be93da7855fe6695fc155059d1908f0a8c6d
prodipdev <prodipink@gmail.com>
2023-08-16 00:13:27 +0600

    Some modify

--- client-side/src/pages/Review/Review/Review.jsx
+++ client-side/src/pages/Review/Review/Review.jsx
@@ -3,12 +3,12 @@ import FareSummary from "../FareSummary/FareSummary";
 const Review = () => {
   return (
     <div className="mt-16 max-w-7xl mx-auto px-5 sm:px-10">
-      <div className="grid md:grid-cols-3 gap-5">
+      <div className="grid md:grid-cols-3 gap-10">
         <div className="col-span-2">
           <div>Tracking Navigation </div>
           <div className="mt-8">Left Side</div>
         </div>
-        <div className="col-span-1">
+        <div className="col-span-1 mt-14">
           <FareSummary />
         </div>
       </div>
