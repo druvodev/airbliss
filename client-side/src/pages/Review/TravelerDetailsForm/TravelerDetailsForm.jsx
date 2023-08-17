be5bcb203f494f599faffc99c00cf06a0bb4853c
Zahangir Alam Rafe <rafe.z@standard-group.com>
2023-08-17 11:16:07 +0600

    Form Design Done

--- client-side/src/pages/Review/TravelerDetailsForm/TravelerDetailsForm.jsx
+++ client-side/src/pages/Review/TravelerDetailsForm/TravelerDetailsForm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
-import { FaExclamationCircle, FaInfoCircle } from "react-icons/fa";
+import { FaExclamationCircle } from "react-icons/fa";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 
@@ -15,10 +15,10 @@ const TravelerDetailsForm = () => {
   }, []);
 
   return (
-    <div className="">
+    <div className="relative">
       <div className="shadow-md rounded-xl overflow-hidden">
         <div className="px-5 ">
-          <div className="flex items-center gap-2 mb-3">
+          <div className="flex items-center gap-2 mb-8">
             <h2 className="text-2xl font-semibold">Traveler 1</h2>
             <span className="px-2 py-1 border bg-[#e4dede] rounded text-sm ">
               Adult
@@ -26,7 +26,7 @@ const TravelerDetailsForm = () => {
             <span className="font-semibold text-gray-600">Primary Contact</span>
           </div>
           <hr />
-          <div className="text-end -mt-10">
+          <div className=" absolute top-5 right-5">
             <button onClick={() => setIsCollapse(!isCollapse)}>
               {isCollapse ? (
                 <MdKeyboardArrowUp className="text-2xl rounded-full bg-gray-300" />
@@ -37,8 +37,11 @@ const TravelerDetailsForm = () => {
           </div>
         </div>
         <div
+          // className={`duration-500 ${
+          //   !isCollapse && "max-h-0"
+          // } transition-all ease-linear`}
           className={`duration-500 ${
-            !isCollapse && "max-h-0"
+            isCollapse ? "max-h-[1275px] md:max-h-[925px]" : "max-h-0"
           } transition-all ease-linear overflow-hidden`}
         >
           <div className="p-5">
@@ -192,11 +195,16 @@ const TravelerDetailsForm = () => {
                   />
                 </div>
                 <div className="flex flex-col">
-                  <label className="font-semibold">
+                  <label className="font-semibold mb-1">
                     Phone Number<span className="text-red-600">*</span>
                   </label>
                   <PhoneInput
                     country={"bd"}
+                    inputStyle={{
+                      width: "100%",
+                      padding: "20px 40px",
+                      border: "1px solid rgba(158, 158, 158,0.2)",
+                    }}
                     value={this?.state.phone}
                     onChange={(phone) => this?.setState({ phone })}
                     inputProps={{
