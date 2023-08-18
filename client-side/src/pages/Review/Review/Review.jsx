2b6dc5407066eb9f4feb2a62043691ea8b175438
Zahangir Alam Rafe <rafe.z@standard-group.com>
2023-08-18 17:27:04 +0600

    input form submit and console done

--- client-side/src/pages/Review/Review/Review.jsx
+++ client-side/src/pages/Review/Review/Review.jsx
@@ -3,14 +3,13 @@ import TravelerDetailsForm from "../TravelerDetailsForm/TravelerDetailsForm";
 
 const Review = () => {
   return (
-    <div className="mt-16 max-w-7xl mx-auto px-5 sm:px-10">
+    <div className="m-16 max-w-7xl mx-auto px-5 sm:px-10 pb-20">
       <div className="grid md:grid-cols-3 gap-10">
         <div className="md:col-span-2">
           <div>Tracking Navigation </div>
           <div className="mt-8">
             <TravelerDetailsForm />
           </div>
-          <div className="mt-8">Left Side</div>
         </div>
         <div className="md:col-span-1 mt-14">
           <FareSummary />
