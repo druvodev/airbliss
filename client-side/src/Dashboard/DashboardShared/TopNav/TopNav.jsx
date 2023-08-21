f98cef5df4c5156d58ef4111e43d7521460a4eb5
AbdullahWB <abdullah917828@gmail.com>
2023-08-21 13:14:29 +0600

    make top nav

---
+++ client-side/src/Dashboard/DashboardShared/TopNav/TopNav.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import useAuth from '../../../hooks/useAuth';
+
+const TopNav = () => {
+    const { user } = useAuth()
+    return (
+        <>
+            <div className="navbar justify-between px-10 bg-cyan-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                    </div>
+                </div>
+                <div className="navbar-end">
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src={user?.photoURL} />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                    <div className=''>
+                        <h4 className='mx-2 mt-2 font-medium text-[18px] text-gray-800  hover:underline'>
+                            {user?.displayName}
+                        </h4>
+                        <p className='mx-2 mt-1 text-[14px] text-sm font-medium text-gray-600  hover:underline'>
+                            {user?.email}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default TopNav;
--- client-side/src/layouts/DashboardLayout.jsx
+++ client-side/src/layouts/DashboardLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../Dashboard/Sidebar/Sidebar';
+import TopNav from '../Dashboard/DashboardShared/TopNav/TopNav';
 
 const DashboardLayout = () => {
     return (
         <div className='relative min-h-screen md:flex'>
             <Sidebar />
             <div className='flex-1 md:ml-64'>
+                <TopNav />
                 <div className='p-5'>
                     <Outlet />
                 </div>
