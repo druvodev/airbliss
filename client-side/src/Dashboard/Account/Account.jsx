44aaeb81a7a1dbb6032030285553e479bfa95812
AbdullahWB <abdullah917828@gmail.com>
2023-08-22 13:38:00 +0600

    restyle and add some components

---
+++ client-side/src/Dashboard/Account/Account.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Account = () => {
+    return (
+        <div>
+            This For Account
+        </div>
+    );
+};
+
+export default Account;
---
+++ client-side/src/Dashboard/Admin/ManageUsers/ManageUsers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageUsers = () => {
+    return (
+        <div>
+            This For ManageUsers
+        </div>
+    );
+};
+
+export default ManageUsers;
