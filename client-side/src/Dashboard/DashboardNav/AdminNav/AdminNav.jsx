d041962688c089e497bb766c92aa3872c1b6ee96
AbdullahWB <abdullah917828@gmail.com>
2023-08-28 21:48:48 +0600

    make user managment page

--- client-side/src/Dashboard/DashboardNav/AdminNav/AdminNav.jsx
+++ client-side/src/Dashboard/DashboardNav/AdminNav/AdminNav.jsx
@@ -8,20 +8,19 @@ const AdminNav = () => {
       <NavLink
         to="adminHome"
         className={({ isActive }) =>
-          `flex items-center rounded-md px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-cyan-300   hover:text-gray-700 ${
-            isActive ? "bg-cyan-300  text-gray-700" : "text-gray-600"
+          `flex items-center rounded-full px-4 py-2 mt-5 transition-colors duration-300 transform hover:bg-cyan-300 hover:text-gray-700 ${isActive
+            ? 'bg-white text-gray-700 active:border rounded-full'
+            : 'text-gray-600'
           }`
         }
       >
         <FaHouseUser className="w-5 h-5" />
-
         <span className="mx-4 font-medium">Admin Home</span>
       </NavLink>
       <NavLink
         to="manageUsers"
         className={({ isActive }) =>
-          `flex items-center rounded-md px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-cyan-300   hover:text-gray-700 ${
-            isActive ? "bg-cyan-300  text-gray-700" : "text-gray-600"
+          `flex items-center rounded-md px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-cyan-300   hover:text-gray-700 ${isActive ? "bg-cyan-300  text-gray-700" : "text-gray-600"
           }`
         }
       >
@@ -32,8 +31,7 @@ const AdminNav = () => {
       <NavLink
         to="addFlight"
         className={({ isActive }) =>
-          `flex items-center rounded-md px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-cyan-300   hover:text-gray-700 ${
-            isActive ? "bg-cyan-300  text-gray-700" : "text-gray-600"
+          `flex items-center rounded-md px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-cyan-300   hover:text-gray-700 ${isActive ? "bg-cyan-300  text-gray-700" : "text-gray-600"
           }`
         }
       >
@@ -44,8 +42,7 @@ const AdminNav = () => {
       <NavLink
         to="flightStatus"
         className={({ isActive }) =>
-          `flex items-center rounded-md px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-cyan-300   hover:text-gray-700 ${
-            isActive ? "bg-cyan-300  text-gray-700" : "text-gray-600"
+          `flex items-center rounded-md px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-cyan-300   hover:text-gray-700 ${isActive ? "bg-cyan-300  text-gray-700" : "text-gray-600"
           }`
         }
       >
@@ -56,8 +53,7 @@ const AdminNav = () => {
       <NavLink
         to="account"
         className={({ isActive }) =>
-          `flex items-center rounded-md px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-cyan-300   hover:text-gray-700 ${
-            isActive ? "bg-cyan-300  text-gray-700" : "text-gray-600"
+          `flex items-center rounded-md px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-cyan-300   hover:text-gray-700 ${isActive ? "bg-cyan-300  text-gray-700" : "text-gray-600"
           }`
         }
       >
@@ -69,8 +65,7 @@ const AdminNav = () => {
       <NavLink
         to="/"
         className={({ isActive }) =>
-          `flex items-center rounded-md px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-cyan-300   hover:text-gray-700 ${
-            isActive ? "bg-cyan-300  text-gray-700" : "text-gray-600"
+          `flex items-center rounded-md px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-cyan-300   hover:text-gray-700 ${isActive ? "bg-cyan-300  text-gray-700" : "text-gray-600"
           }`
         }
       >
