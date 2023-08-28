b86b6f042fa0cbed4a262d56f607314f2e01880b
jishan1019 <imranislamjishan200@gmail.com>
2023-08-28 18:46:39 +0600

    dinamic titel show

--- server-side/index.js
+++ server-side/index.js
@@ -106,6 +106,20 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/single_flights/:id", async (req, res) => {
+      const id = req.params.id;
+      const formAirportCode = req.query.airportCode;
+      const findFlight = await flightsCollection.find().toArray();
+      const singleFlight = findFlight.find(
+        (flight) => flight._id.toString() === id
+      );
+
+      console.log(singleFlight);
+
+      // const result = await flightsCollection.find().toArray();
+      // res.send(result);
+    });
+
     app.post("/add_flight/:id", async (req, res) => {
       const id = req.params.id;
       const formAirportCode = req.query.airportCode;
