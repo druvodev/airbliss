b792097b0f43214f22d891766a3213c3e5ffdb85
jishan1019 <imranislamjishan200@gmail.com>
2023-08-31 19:45:00 +0600

    time date seat show

--- client-side/src/pages/Flights/BookingFlights/FareRuls/FareRuls.jsx
+++ client-side/src/pages/Flights/BookingFlights/FareRuls/FareRuls.jsx
@@ -8,23 +8,21 @@ const FareRuls = ({ flightFullDetails }) => {
     <section className="mt-3 border-[1px] rounded-sm">
       <div className=" p-2 border-b-[1px] bg-gray-100 flex gap-3 items-center">
         <img className="h-6 w-6" src={flightFullDetails?.airlineLogo} alt="" />
-        <h1>
-          <b>
-            {city} to {arrive?.city}
-          </b>
+        <h1 className="font-semibold">
+          {city} to {arrive?.city}
         </h1>
       </div>
 
       {/* Table Body */}
       <section className="p-3">
         <div>
-          <h4 className="text-[13px]">
-            <strong>Some ruls and regulations:</strong>
-          </h4>
+          <h4 className="text-[13px] font-bold">Some ruls and regulations:</h4>
 
-          <div>
+          <div className="mt-2">
             {flightFullDetails.notes.map((note, index) => (
-              <p key={index}>{note}</p>
+              <p className="text-[14px]" key={index}>
+                {note}
+              </p>
             ))}
           </div>
         </div>
