1ae2bb49e476bb399ac29d4cae8f188fd8e3665c
AbdullahWB <abdullah917828@gmail.com>
2023-09-04 21:22:18 +0600

    make total refund

--- client-side/src/Dashboard/User/UserHome/TotalBooking.jsx
+++ client-side/src/Dashboard/User/UserHome/TotalBooking.jsx
@@ -4,7 +4,7 @@ import { BiSolidBookmarkAltPlus } from 'react-icons/bi';
 const TotalBooking = () => {
     return (
         <div className='bg-[#176B87] p-6 rounded-lg'>
-            <h1 className='text-2xl font-semibold text-white'>Total Booking</h1>
+            <h1 className='text-2xl font-semibold text-white'>Total Booking Amount</h1>
             <div className='flex justify-between mt-4 gap-2'>
                 <p className='text-white text-3xl font-bold'>200$</p>
                 <BiSolidBookmarkAltPlus className='text-white text-3xl font-bold mt-1' />
--- client-side/src/Dashboard/User/UserHome/TotalRefund.jsx
+++ client-side/src/Dashboard/User/UserHome/TotalRefund.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { RiRefund2Fill } from 'react-icons/ri';
 
 const TotalRefund = () => {
     return (
-        <div>
-            TotalRefund 
+        <div className='bg-[#7A316F] p-6 rounded-lg'>
+            <h1 className='text-2xl font-semibold text-white'>Total Refund Amount</h1>
+            <div className='flex justify-between mt-4 gap-2'>
+                <p className='text-white text-3xl font-bold'>50$</p>
+                <RiRefund2Fill className='text-white text-3xl font-bold mt-1' />
+            </div>
         </div>
     );
 };
