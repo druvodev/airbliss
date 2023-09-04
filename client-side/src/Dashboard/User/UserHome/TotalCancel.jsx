a2362b47b195b595278e03d06b38f0ac00b3418d
AbdullahWB <abdullah917828@gmail.com>
2023-09-04 21:18:39 +0600

    make total cancel

--- client-side/src/Dashboard/User/UserHome/TotalCancel.jsx
+++ client-side/src/Dashboard/User/UserHome/TotalCancel.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { TbCalendarCancel } from 'react-icons/tb';
 
 const TotalCancel = () => {
     return (
-        <div>
-            TotalCancel 
+        <div className='bg-[#614BC3] p-6 rounded-lg'>
+            <h1 className='text-2xl font-semibold text-white'>Total Canceling Ticket</h1>
+            <div className='flex justify-between mt-4 gap-2'>
+                <p className='text-white text-3xl font-bold'>4 times</p>
+                <TbCalendarCancel className='text-white text-3xl font-bold mt-1' />
+            </div>
         </div>
     );
 };
