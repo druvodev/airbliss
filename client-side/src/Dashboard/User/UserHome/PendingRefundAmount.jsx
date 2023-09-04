4edda6da21e38f3087b71c555c9086edc9708a40
AbdullahWB <abdullah917828@gmail.com>
2023-09-04 21:25:40 +0600

    make pending refund

--- client-side/src/Dashboard/User/UserHome/PendingRefundAmount.jsx
+++ client-side/src/Dashboard/User/UserHome/PendingRefundAmount.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { RiRefundFill } from 'react-icons/ri';
 
 const PendingRefundAmount = () => {
     return (
-        <div>
-            PendingRefundAmount
+        <div className='bg-[#FF8551] p-6 rounded-lg'>
+            <h1 className='text-2xl font-semibold text-white'>Pending Refund Amount</h1>
+            <div className='flex justify-between mt-4 gap-2'>
+                <p className='text-white text-3xl font-bold'>100$</p>
+                <RiRefundFill className='text-white text-3xl font-bold mt-1' />
+            </div>
         </div>
     );
 };
