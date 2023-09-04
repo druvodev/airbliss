33841d0850b9b404ffd902486639718d2ee28f95
AbdullahWB <abdullah917828@gmail.com>
2023-09-04 21:37:59 +0600

    make total amount

--- client-side/src/Dashboard/User/UserHome/TotalAmount.jsx
+++ client-side/src/Dashboard/User/UserHome/TotalAmount.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { FaMoneyBillTrendUp } from 'react-icons/fa6';
 
 const TotalAmount = () => {
     return (
-        <div>
-            TotalAmount
+        <div className='bg-[#42C2FF] p-6 rounded-lg'>
+        <h1 className='text-2xl font-semibold text-white'>Total Booking Amount</h1>
+        <div className='flex justify-between mt-4 gap-2'>
+            <p className='text-white text-3xl font-bold'>200$</p>
+            <FaMoneyBillTrendUp className='text-white text-3xl font-bold mt-1' />
         </div>
+    </div>
     );
 };
 
