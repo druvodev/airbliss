904b0afe49231095c559b6f23be90d4c417a1f82
AbdullahWB <abdullah917828@gmail.com>
2023-09-05 16:54:45 +0600

    change style user cart

--- client-side/src/Dashboard/User/UserHome/TotalBooking.jsx
+++ client-side/src/Dashboard/User/UserHome/TotalBooking.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { BiSolidBookmarkAltPlus } from 'react-icons/bi';
+import { SlOptionsVertical } from 'react-icons/sl';
 
 const TotalBooking = () => {
     return (
-        <div className='bg-white shadow-md shadow-lg p-6 rounded-lg'>
-            <h1 className='text-2xl font-semibold text-gray-900'>Total Ticket Booking</h1>
-            <div className='flex justify-between mt-4 gap-2'>
-                <p className='text-gray-900 text-3xl font-bold'>30 times</p>
-                <BiSolidBookmarkAltPlus className='text-gray-900 text-3xl font-bold mt-1' />
+        <div className='bg-white shadow-md p-6 rounded-lg flex justify-between items-center'>
+            <div className='flex items-center gap-10'>
+                <div className='border-[#FF8551] border-4 bg-[rgba(255,133,81,0.35)] w-16 h-16 rounded-full justify-center items-center flex'>
+                    <BiSolidBookmarkAltPlus className='text-white text-4xl font-bold' />
+                </div>
+                <div>
+                    <h1 className='text-2xl font-light text-gray-600'>Total Ticket Booking</h1>
+                    <p className='text-[#96a3c4] text-3xl font-semibold'>30 times</p>
+                </div>
+            </div>
+            <div>
+                <button>
+                    <SlOptionsVertical className='text-gray-400 text-xl font-bold mt-1' />
+                </button>
             </div>
         </div>
     );
