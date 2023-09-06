26d53494703b3273a3525f7fbcb41e577c131ab8
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-09-07 00:43:03 +0600

    fix error and add cancel flight modal

--- client-side/src/redux/features/usersSlice.js
+++ client-side/src/redux/features/usersSlice.js
@@ -1,18 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    userInfo: {},
-}
+  userInfo: {},
+  userBookings: [],
+  flightRef: "",
+};
 
 export const usersSlice = createSlice({
-    name: "userInfo",
-    initialState,
-    reducers: {
-        setUserInfo: (state, action) => {
-            state.userInfo = action.payload;
-        }
-    }
-})
+  name: "userInfo",
+  initialState,
+  reducers: {
+    setUserInfo: (state, action) => {
+      state.userInfo = action.payload;
+    },
+    setUserBookings: (state, action) => {
+      state.userBookings = action.payload;
+    },
+    setFlightRef: (state, action) => {
+      state.userBookings = action.payload;
+    },
+  },
+});
 
-export const { setUserInfo } = usersSlice.actions;
+export const { setUserInfo, setUserBookings, setFlightRef } =
+  usersSlice.actions;
 export default usersSlice.reducer;
