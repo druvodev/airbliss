955f03ce184bc2a0a383e9b6feb3b67074f45f08
AbdullahWB <abdullah917828@gmail.com>
2023-09-07 10:54:46 +0600

    rsponsive user home

--- client-side/src/Dashboard/User/UserHome/BookingCalendar.jsx
+++ client-side/src/Dashboard/User/UserHome/BookingCalendar.jsx
@@ -8,9 +8,9 @@ const BookingCalendar = () => {
     ]);
 
     return (
-        <div className='bg-white shadow-md p-6 rounded-lg h-full'>
+        <div className='bg-white shadow-md lg:p-6 p-2 rounded-lg h-full'>
             <div className='flex justify-between mb-10'>
-                <h1 className='text-2xl font-light text-gray-600'>Booking Calendar</h1>
+                <h1 className='lg:text-2xl font-light text-gray-600'>Booking Calendar</h1>
                 <button>
                     <SlOptionsVertical className='text-gray-400 text-xl font-bold mt-1' />
                 </button>
@@ -22,7 +22,7 @@ const BookingCalendar = () => {
                     direction="vertical"
                     showDateDisplay={false}
                     minDate={new Date()}
-                    className="w-fit shadow rounded-md"
+                    className="w-[320px] lg:w-fit lg:shadow rounded-md"
                     booked={bookedDates.map(date => new Date(date))}
                 />
             </div>
