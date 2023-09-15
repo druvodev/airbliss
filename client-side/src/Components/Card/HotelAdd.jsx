df4108103db7b0ab9cafc616b26469c68a8a1af2
HasibulHasan <hasibulhasan372@gmail.com>
2023-09-15 15:47:06 +0600

    Conflict Resolve image height fixed

--- client-side/src/Components/Card/HotelAdd.jsx
+++ client-side/src/Components/Card/HotelAdd.jsx
@@ -4,7 +4,7 @@ const HotelAdd = ({hotel}) => {
     const { name, details, star, distance, cost, discount, img, id } = hotel;
     return (
         <div className="shadow-2xl p-4 rounded-lg">
-        <img src={img} alt="" />
+        <img src={img} alt="" className="object-cover mx-auto h-56 shadow-md rounded-md" />
         <div className="space-y-3 pt-5 pl-2">
           <h3 className="lg:text-2xl xl:text-2xl text-[#504D75] ">{name}</h3>
           <p className="flex gap-x-1 items-center text-[#8B89A4]"><CiLocationOn></CiLocationOn> {distance}</p>
