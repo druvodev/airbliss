e0c9befe88a1e861f072ca2014185262396992d1
prodipdev <prodipink@gmail.com>
2023-09-22 19:54:29 +0600

    Modify server

--- server-side/index.js
+++ server-side/index.js
@@ -634,10 +634,10 @@ async function run() {
         total_amount: totalAmount,
         currency: "BDT",
         tran_id: transitionId,
-        success_url: `https://server-side-tawny-sigma.vercel.app/booking-confirmed/${bookingInfo.bookingReference}`,
-        fail_url: "https://server-side-tawny-sigma.vercel.app/booking-failed",
-        cancel_url: "https://server-side-tawny-sigma.vercel.app/booking-cancel",
-        ipn_url: "https://server-side-tawny-sigma.vercel.app/ipn",
+        success_url: `http://localhost:5000/booking-confirmed/${bookingInfo.bookingReference}`,
+        fail_url: "http://localhost:5000/booking-failed",
+        cancel_url: "http://localhost:5000/booking-cancel",
+        ipn_url: "http://localhost:5000/ipn",
         shipping_method: "Air Flights",
         product_name: "Airline Ticket",
         product_category: "Flights Tickets",
@@ -699,7 +699,7 @@ async function run() {
         });
       app.post("/booking-confirmed/:bookingId", async (req, res) => {
         res.redirect(
-          `https://airbliss-flight-booking.web.app/booking-confirmed/${req.params.bookingId}`
+          `http://localhost:5173/booking-confirmed/${req.params.bookingId}`
         );
       });
     });
@@ -1083,6 +1083,7 @@ async function run() {
             $set: {
               [`${date}.${airportCode}.$.residualStatus`]: rescheduleStatus,
               [`${date}.${airportCode}.$.user.seatNo`]: seatNo,
+              [`${date}.${airportCode}.$.flight.departureDate`]: flightDate,
             },
           };
 
